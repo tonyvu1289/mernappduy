744d261800407e86231112072c37622dd9759b4d
tonyvu1289 <tonyvu1289@gmail.com>
2022-04-14 12:58:40 +0700

    goal controller and router setup

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,36 @@
+const asyncHandler = require('express-async-handler')
+//@ desc    Get goals
+//@route    GET /api/goals
+//@access   Private
+const getGoals = asyncHandler(async (req,res) => {
+    res.status(200).json({message: "get goal"})  
+})
+
+//@ desc    Set goals
+//@route    POST /api/goals
+//@access   Private
+const setGoals = asyncHandler(async(req,res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add text field')
+    }
+    res.status(200).json({message: "set goal"})  
+})
+//@ desc    update goals
+//@route    PUT /api/goals/:id
+//@access   Private
+const updateGoals = asyncHandler(async(req,res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+})
+//@ desc    delte goals
+//@route    DELETE /api/goals
+//@access   Private
+const deleteGoals = asyncHandler(async (req,res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+})
+module.exports = {
+    getGoals,
+    setGoals,
+    updateGoals,
+    deleteGoals,
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {
+    errorHandler
+} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000 
+
+// create server
+const app = express()
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use('/api/goals',require('./routes/goalRoutes'))
+app.use(errorHandler)
+// start listen
+app.listen(port,() => console.log('Server start on port',port))
+
+
