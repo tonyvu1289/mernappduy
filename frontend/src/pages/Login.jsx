534b66808278977336d556b1aec158882f2fd1af
tonyvu1289 <tonyvu1289@gmail.com>
2022-04-22 16:38:17 +0700

    added frontend UI

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+    const {email, password} = formData
+    const onChange = (e) => {
+        setFormData((prevState)=> ({
+            ...prevState,
+            [e.target.name] : e.target.value,
+        }))
+     }
+
+    const onSubmit = (e) =>{
+        e.preventDefault()
+    } 
+    return (
+        <div>
+            <section className="heading">
+                <h1>
+                    <FaSignInAlt /> Login
+                </h1>
+                <p> Please login</p>
+
+            </section>
+            <section className="form">
+                <form onSubmit = {onSubmit}>
+        
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            name='email'
+                            value={email}
+                            placeholder='Enter your email'
+                            onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            name='password'
+                            value={password}
+                            placeholder='Enter your password'
+                            onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <buton type="submit" className= 'btn btn-block'>
+                            Submit
+                            </buton>
+                    </div>
+                </form>
+            </section>
+        </div>
+    )
+}
+
+export default Login
